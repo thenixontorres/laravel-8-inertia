1c4996382e46fd28ee5b79cbdb65691676d8b005
thenixontorres <nixontorres2@gmail.com>
2021-02-06 14:15:04 -0400

    lesson 8

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Note;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'email' => 'i@admin.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        Note::factory(30)->create();    
     }
 }
